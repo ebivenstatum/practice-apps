7269a60a73a63915fe0b42a41544721b9cdc4297
Elizabeth Bivens-Tatum <ebivenstatum@gmail.com>
2022-06-29 16:36:44 -0400

    Preliminary server and db setup, will be tested for bugs when other base functionality is available

--- 1-glossary/server/db.js
+++ 1-glossary/server/db.js
@@ -1,15 +1,13 @@
 const mongoose = require("mongoose");
+const config = reqire("./server/.env");
 
 // 1. Use mongoose to establish a connection to MongoDB
 // 2. Set up any schema and models needed by the app
 // 3. Export the models
 // 4. Import the models into any modules that need them
 
-let port = 3000;
-let file = "/glossary"
-
 mongoose
-  .connect(`mongodb://127.0.0.1:${port}${file}`)
+  .connect(`mongodb://127.0.0.1:${config.PORT}/${config.DB_NAME}`)
   .then(() => console.log("Database Connected"))
   .catch(err => console.log(err));
 
@@ -29,10 +27,16 @@ const Glossary = new mongoose.model("Glossary", glossarySchema);
 
 const create = (data) => {
   // takes term and def as data
+  return Glossary.create(data).exec();
 };
 
 const find = (term) => {
   // if search term does not exist, return all db entries
+  if (term) {
+    return Glossary.find({term}).exec();
+  } else {
+    return Glossary.find().exec();
+  }
 };
 
 module.exports = {
--- 1-glossary/server/index.js
+++ 1-glossary/server/index.js
@@ -1,19 +1,31 @@
 require("dotenv").config();
 const express = require("express");
 const path = require("path");
+const db = require("./server/db.js"); // import mongoose db
 
 const app = express();
 
 // Serves up all static and generated assets in ../client/dist.
 app.use(express.static(path.join(__dirname, "../client/dist")));
 
-/**** 
- * 
- * 
+/****
+ *
+ *
  * Other routes here....
  *
- * 
+ *
  */
+app.post(file, function(req, res) {
+  app.create(req.body).then(data => {
+    res.sendStatus();
+  });
+});
+
+app.get(file, function(req, res) {
+  db.find(req.body).then(items => {
+    return res.json(items);
+  });
+});
 
 app.listen(process.env.PORT);
 console.log(`Listening at http://localhost:${process.env.PORT}`);
